991278c63dd856093e3535759bc8d100e95009f4
EverettKey <everett.k.key@gmail.com>
2019-10-17 21:39:51 +0800

    each frame takes about 0.1 second to process, click action is slow, too

--- main.py
+++ main.py
@@ -16,30 +16,37 @@ score = 0
 previous_column = x_positions[0]
 
 # screen = np.array(Image)
+previous_column = -1
 
-
-def clickOnBlack(screen):
-    global gameCoords, score, w, h, x_positions, previous_column
-    for y in reversed(range(h)):
+def clickOnBlack(screen, w, h, x_positions):
+    global previous_column
+    x0 = 1140
+    y0 = 150
+    for y in reversed(range(0, h, 5)):
         for x in x_positions:
             if x != previous_column:
                 if screen[y][x] < 40:
-                    actual_y = int(game_coords[1] + y)
-                    actual_x = int(game_coords[0] + x)
+                    actual_y = y0 + y
+                    actual_x = x0 + x
                     click(actual_x, actual_y)
                     previous_column = x
                     print('clicked at {}, {}'.format(actual_x, actual_y))
-                    return
+                    return 1
+    return 0
 
             
 time.sleep(1)
 while(True):
     cursor = queryMousePosition()
     
+    
     if game_coords[0] < cursor.x < game_coords[2]:
+        startTime = time.time()
         screen = np.array(ImageGrab.grab(bbox=game_coords))
         screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
-        clickOnBlack(screen)
+        score += clickOnBlack(screen, w, h, x_positions)
+        print("processing this frame took {} second".format(time.time()-startTime))
         
     else:
+        print("score:{}".format(score))
         break
